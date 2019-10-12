04ce8c341b40323bd5c103a3631071d88f4b17dd
Peter Beens <pbeens@gmail.com>
2019-10-12 14:35:42 -0400

    Added a download utility

---
+++ Download-Utility/download.sh
@@ -0,0 +1,10 @@
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains www.edu.gov.mb.ca \
+     --no-parent \
+         https://www.edu.gov.mb.ca/k12/cur/index.html
